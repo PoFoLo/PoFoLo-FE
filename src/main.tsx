b7fd73671205a732901e21a69a5e4466ea9544ad
letthem <suyeon3662@naver.com>
2024-11-02 13:12:44 +0900

    [Design] color & font 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App.tsx';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles.ts';
+import theme from './styles/theme.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <ThemeProvider theme={theme}>
     <GlobalStyles />
     <App />
-  </StrictMode>
+  </ThemeProvider>
 );
---
+++ src/styles/theme.ts
@@ -0,0 +1,131 @@
+const theme = {
+  colors: {
+    white: '#FFFFFF',
+    gray5: '#F8F8F8',
+    gray10: '#F2F2F3',
+    gray20: '#E4E4E6',
+    gray30: '#C9CACF',
+    gray40: '#BFC0C5',
+    gray50: '#B0B1B6',
+    gray60: '#919298',
+    gray70: '#74757A',
+    gray80: '#57585B',
+    gray90: '#303133',
+    blue10: '#E7EFFF',
+    blue20: '#CBDCFF',
+    blue30: '#A6C3FD',
+    blue40: '#80A9FC',
+    blue50: '#598DF6',
+    blue60: '#3270ED',
+    green10: '#DDF5DD',
+    green50: '#1EBE1E',
+    purple10: '#EFE3FF',
+    purple50: '#9747FF',
+    coral10: '#FFEAE6',
+    coral50: '#FF715B',
+    orange10: '#FFEDD9',
+    orange50: '#FF8800',
+    yellow10: '#FFF4DC',
+    yellow50: '#FFB815',
+  },
+  fonts: {
+    headline1: `
+      font-family: 'Pretendard', sans-serif;;
+      font-size: 4rem;
+      font-weight: 700;
+      font-style: normal;
+      line-height: 130%;
+      letter-spacing: 0.02rem;
+    `,
+    headline2: `
+      font-family: 'Pretendard', sans-serif;;
+      font-size: 3.2rem;
+      font-weight: 700;
+      font-style: normal;
+      line-height: 130%;
+      letter-spacing: 0.016rem;
+    `,
+    subhead1: `
+      font-family: 'Pretendard', sans-serif;;
+      font-size: 2.8rem;
+      font-weight: 600;
+      font-style: normal;
+      line-height: 150%;
+      letter-spacing: 0.007rem;
+    `,
+    subhead2: `
+      font-family: 'Pretendard', sans-serif;;
+      font-size: 2rem;
+      font-weight: 600;
+      font-style: normal;
+      line-height: 150%;
+      letter-spacing: 0.005rem;
+    `,
+    subhead3: `
+      font-family: 'Pretendard', sans-serif;
+      font-size: 1.6rem;
+      font-weight: 600;
+      font-style: normal;
+      line-height: 150%;
+      letter-spacing: 0.004rem;
+    `,
+    body1: `
+      font-family: 'Pretendard', sans-serif;
+      font-size: 2.1rem;
+      font-weight: 400;
+      font-style: normal;
+      line-height: 150%;
+      letter-spacing: 0.0105rem;
+    `,
+    body2: `
+      font-family: 'Pretendard', sans-serif;
+      font-size: 1.8rem;
+      font-weight: 400;
+      font-style: normal;
+      line-height: 150%;
+      letter-spacing: 0.009rem;
+    `,
+    body3: `
+      font-family: 'Pretendard', sans-serif;
+      font-size: 1.6rem;
+      font-weight: 400;
+      font-style: normal;
+      line-height: 150%;
+      letter-spacing: 0.008rem;
+      `,
+    caption1: `
+      font-family: 'Pretendard', sans-serif;
+      font-size: 1.8rem;
+      font-weight: 600;
+      font-style: normal;
+      line-height: 150%;
+      letter-spacing: 0.0045rem;
+      `,
+    caption2: `
+      font-family: 'Pretendard', sans-serif;
+      font-size: 1.6rem;
+      font-weight: 600;
+      font-style: normal;
+      line-height: 140%;
+      letter-spacing: 0.004rem;
+      `,
+    caption3: `
+      font-family: 'Pretendard', sans-serif;
+      font-size: 1.4rem;
+      font-weight: 600;
+      font-style: normal;
+      line-height: 140%;
+      letter-spacing: 0.0035rem;
+      `,
+    caption4: `
+      font-family: 'Pretendard', sans-serif;
+      font-size: 1.2rem;
+      font-weight: 500;
+      font-style: normal;
+      line-height: 140%;
+      letter-spacing: 0.003rem;
+      `,
+  },
+};
+
+export default theme;
