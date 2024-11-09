c0178644a337ac306e7cc6d0fd4dd86404416882
letthem <suyeon3662@naver.com>
2024-11-09 19:02:31 +0900

    [Feat] 댓글 입력 UI 구현

--- src/components/ProjectDetail/Comment/index.tsx
+++ src/components/ProjectDetail/Comment/index.tsx
@@ -1,5 +1,53 @@
 import * as S from '@/components/ProjectDetail/Comment/styles';
+import profileIcon from '@/assets/webps/Common/profileIcon.webp';
+import Button from '@/components/Common/Button';
+import { useEffect, useRef, useState } from 'react';
 
 export const Comment = () => {
-  return <S.CommentContainer></S.CommentContainer>;
+  const [comment, setComment] = useState('');
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleCommentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(e.target.value);
+  };
+
+  // textarea 높이 조정 함수
+  const adjustHeight = () => {
+    if (textAreaRef.current) {
+      textAreaRef.current.style.height = '5.6rem'; // default
+      textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+    }
+  };
+
+  useEffect(() => {
+    adjustHeight();
+  }, [comment]);
+
+  return (
+    <S.CommentLayout>
+      <S.CommentContainer>
+        <S.CommentTitle>
+          <h2>프로젝트 댓글 </h2>
+          <span>5</span>
+        </S.CommentTitle>
+
+        <S.AddComment>
+          <img src={profileIcon} alt="profile icon" />
+          <S.CommentBox>
+            <S.CommentTextArea
+              ref={textAreaRef}
+              placeholder="댓글"
+              value={comment}
+              onChange={handleCommentChange}
+            />
+            <S.ButtonWrapper>
+              <Button size="small" type={comment.trim() ? 'main' : 'inactive'}>
+                게시
+              </Button>
+            </S.ButtonWrapper>
+          </S.CommentBox>
+        </S.AddComment>
+      </S.CommentContainer>
+    </S.CommentLayout>
+  );
 };
--- src/components/ProjectDetail/Comment/styles.ts
+++ src/components/ProjectDetail/Comment/styles.ts
@@ -1,7 +1,82 @@
 import styled from 'styled-components';
 
-export const CommentContainer = styled.div`
+export const CommentLayout = styled.div`
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   background-color: ${(props) => props.theme.colors.gray5};
   border-top: 0.1rem solid ${(props) => props.theme.colors.gray20};
+`;
+
+export const CommentContainer = styled.section`
+  width: 100%;
+  max-width: 131.2rem;
+  padding: 3.2rem 0;
+
+  @media (max-width: 1440px) {
+    padding: 3.2rem 6.4rem;
+  }
+`;
+
+export const CommentTitle = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+  margin-bottom: 3.6rem;
+
+  h2 {
+    color: ${(props) => props.theme.colors.gray90};
+    ${(props) => props.theme.fonts.headline3};
+  }
+  span {
+    color: ${(props) => props.theme.colors.gray60};
+    ${(props) => props.theme.fonts.subhead1};
+  }
+`;
+
+export const AddComment = styled.div`
+  display: flex;
+  gap: 2rem;
+
+  img {
+    width: 4rem;
+    height: 4rem;
+    border: 0.15rem solid ${(props) => props.theme.colors.gray20};
+    border-radius: 50%;
+  }
+`;
+
+export const CommentBox = styled.div`
+  width: 100%;
+  position: relative;
+  display: flex;
+  align-items: center;
+`;
+
+export const CommentTextArea = styled.textarea`
+  width: 100%;
+  background-color: ${(props) => props.theme.colors.gray10};
+  outline: none;
+  resize: none;
+  border: 0.15rem solid ${(props) => props.theme.colors.gray20};
+  border-radius: 1.2rem;
+  padding: 1.2rem 8.8rem 1.2rem 2rem;
+  height: 5.6rem;
+  overflow: hidden;
+  ${(props) => props.theme.fonts.body2};
+  color: ${(props) => props.theme.colors.gray90};
+
+  &::placeholder {
+    color: ${(props) => props.theme.colors.gray50};
+  }
+
+  &:focus {
+    border: 0.15rem solid ${(props) => props.theme.colors.blue40};
+  }
+`;
+
+export const ButtonWrapper = styled.div`
+  position: absolute;
+  right: 1.2rem;
 `;
