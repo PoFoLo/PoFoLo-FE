8196fd83da4682278d07dab27f3d99c027548af6
letthem <suyeon3662@naver.com>
2024-11-09 19:49:28 +0900

    [Feat] date-fns 적용하여 댓글 생성 시각 반영

--- src/components/ProjectDetail/Comment/index.tsx
+++ src/components/ProjectDetail/Comment/index.tsx
@@ -1,12 +1,15 @@
+import { useEffect, useRef, useState } from 'react';
+import { format, isToday, isYesterday, isThisWeek, getDay } from 'date-fns';
+import { ko } from 'date-fns/locale';
 import * as S from '@/components/ProjectDetail/Comment/styles';
 import profileIcon from '@/assets/webps/Common/profileIcon.webp';
 import Button from '@/components/Common/Button';
-import { useEffect, useRef, useState } from 'react';
 
 interface CommentItem {
   id: number;
   nickname: string;
   content: string;
+  createdAt: Date;
 }
 
 export const Comment = () => {
@@ -28,10 +31,25 @@ export const Comment = () => {
       id: Date.now(), // 고유한 ID 생성
       nickname: '성춘향',
       content: comment,
+      createdAt: new Date(),
     };
 
     setComments((prevComments) => [...prevComments, newComment]);
     setComment(''); // 댓글 입력란 초기화
+  };
+
+  // 댓글 생성 날짜 포맷팅 함수
+  const formatCommentDate = (date: Date) => {
+    if (isToday(date)) {
+      return '오늘';
+    } else if (isYesterday(date)) {
+      return '어제';
+    } else if (isThisWeek(date)) {
+      const dayOfWeek = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+      return dayOfWeek[getDay(date)];
+    } else {
+      return format(date, 'yyyy/MM/dd', { locale: ko });
+    }
   };
 
   // textarea 높이 조정 함수
@@ -83,7 +101,7 @@ export const Comment = () => {
               <S.CommentContentWrapper>
                 <S.CommentInfo>
                   <p>{item.nickname}</p>
-                  <span>어제</span>
+                  <span>{formatCommentDate(item.createdAt)}</span>
                 </S.CommentInfo>
                 <S.CommentContent>{item.content}</S.CommentContent>
               </S.CommentContentWrapper>
