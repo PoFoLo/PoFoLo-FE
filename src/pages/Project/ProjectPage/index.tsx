3a849b5a422b2e01049a46ce3689ff39c251cf73
letthem <suyeon3662@naver.com>
2024-11-09 17:34:59 +0900

    [Feat] Project Detail 본문 UI 1차 구현

--- src/pages/Project/ProjectPage/index.tsx
+++ src/pages/Project/ProjectPage/index.tsx
@@ -1,5 +1,71 @@
 import * as S from '@/pages/Project/ProjectPage/styles';
+import profileIcon from '@/assets/webps/Common/profileIcon.webp';
+import link from '@/assets/webps/Common/link.webp';
+import { useNavigate } from 'react-router-dom';
 
 export const ProjectPage = () => {
-  return <S.Layout>ProjectPage입니다.</S.Layout>;
+  const nav = useNavigate();
+
+  return (
+    <S.Layout>
+      <S.ProjectContainer>
+        <S.ImgContainer>이미지 들어갈 곳</S.ImgContainer>
+
+        <S.TopInfo>
+          <S.ProfileInfo>
+            <img onClick={() => nav('/mypage')} src={profileIcon} alt="profile icon" />
+            <S.ProfileContent>
+              <p onClick={() => nav('/mypage')} className="nickname">
+                홍길동
+              </p>
+              <p className="school">홍익대학교 컴퓨터공학과</p>
+            </S.ProfileContent>
+          </S.ProfileInfo>
+          <S.Date>2024년 12월 30일</S.Date>
+        </S.TopInfo>
+
+        <S.BodyText>
+          <S.Title>BEGIN AGAIN 82小红书日</S.Title>
+          <S.FieldButton>
+            <span>프론트엔드</span>
+          </S.FieldButton>
+
+          <S.Article>
+            <h2>소개</h2>
+            <span>
+              {`In 2024, Xiaohongshu celebrated its 11th anniversary. Over the past decade, the platform has brought together many life-loving, sincere, and friendly REDers.
+Thanks to these vibrant individuals, we can find like-minded people, gradually forming a community, a city, and various beautiful spaces to gather. Together, we face challenges, share joy, and grow.
+For this 11th anniversary, our theme is “BEGIN AGAIN.” We hope everyone can start anew, discovering and exploring more life inspirations, and together, embrace a brighter future.
+This design is based on the theme “BEGIN AGAIN,” showcasing a variety of letter styles and vibrant colors. Additionally, the summer elements of ‘82’ are personified, symbolizing constant forward movement. The blend of fun and symbolism signifies the continuous evolution and progress of Xiaohongshu.`}
+            </span>
+          </S.Article>
+
+          <S.Article>
+            <h2>주요 스킬</h2>
+            <span>Photoshop, Illustrator, Figma</span>
+          </S.Article>
+
+          <S.Article>
+            <h2>링크</h2>
+            <S.LinkList>
+              <S.LinkContainer>
+                <img src={link} alt="link icon" />
+                <S.LinkBox>
+                  <p className="link-title">BEGIN AGAIN 82小红书...</p>
+                  <p className="web-address">behance.net/</p>
+                </S.LinkBox>
+              </S.LinkContainer>
+              <S.LinkContainer>
+                <img src={link} alt="link icon" />
+                <S.LinkBox>
+                  <p className="link-title">홍길동의 외부링크 - Sitename</p>
+                  <p className="web-address">linkname.link</p>
+                </S.LinkBox>
+              </S.LinkContainer>
+            </S.LinkList>
+          </S.Article>
+        </S.BodyText>
+      </S.ProjectContainer>
+    </S.Layout>
+  );
 };
--- src/pages/Project/ProjectPage/styles.ts
+++ src/pages/Project/ProjectPage/styles.ts
@@ -1,5 +1,148 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Layout = styled.div`
-  
-`
+  width: 100%;
+  margin-top: 6.4rem;
+  display: flex;
+  justify-content: center;
+`;
+
+export const ProjectContainer = styled.main`
+  width: 131.2rem;
+  padding-bottom: 11.2rem;
+`;
+
+export const ImgContainer = styled.section`
+  margin-top: 2.4rem;
+  height: 36rem;
+  background-color: white;
+`;
+
+export const TopInfo = styled.section`
+  margin-top: 3.2rem;
+  height: 4.8rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ProfileInfo = styled.div`
+  display: flex;
+  gap: 1.2rem;
+
+  img {
+    width: 4.8rem;
+    height: 4.8rem;
+    border: 1.5px solid ${(props) => props.theme.colors.gray20};
+    border-radius: 50%;
+    cursor: pointer;
+  }
+`;
+
+export const ProfileContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  .nickname {
+    color: ${(props) => props.theme.colors.gray90};
+    ${(props) => props.theme.fonts.subhead3};
+    cursor: pointer;
+  }
+
+  .school {
+    color: ${(props) => props.theme.colors.gray70};
+    ${(props) => props.theme.fonts.caption3};
+  }
+`;
+
+export const Date = styled.span`
+  color: ${(props) => props.theme.colors.gray60};
+  ${(props) => props.theme.fonts.caption2};
+`;
+
+export const BodyText = styled.section`
+  padding: 0 0.4rem;
+`;
+
+export const Title = styled.h2`
+  margin-top: 1.6rem;
+  color: black;
+  ${(props) => props.theme.fonts.headline2};
+`;
+
+export const FieldButton = styled.div`
+  margin: 0.8rem 0 0.4rem;
+  width: max-content;
+  padding: 0.4rem 1.2rem;
+  border-radius: 0.8rem;
+  background-color: ${(props) => props.theme.colors.green10};
+
+  span {
+    color: ${(props) => props.theme.colors.green50};
+    ${(props) => props.theme.fonts.caption2};
+  }
+`;
+
+export const Article = styled.article`
+  margin-top: 3.2rem;
+
+  h2 {
+    color: black;
+    ${(props) => props.theme.fonts.subhead1};
+    margin-bottom: 0.8rem;
+  }
+  span {
+    color: #404040;
+    ${(props) => props.theme.fonts.body2};
+    white-space: pre-wrap;
+  }
+`;
+
+export const LinkList = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 2rem;
+`;
+
+export const LinkContainer = styled.div`
+  margin-top: 1.2rem;
+  width: 28rem;
+  height: 5.6rem;
+  padding: 0.4rem 1.2rem;
+  display: flex;
+  gap: 1.2rem;
+  align-items: center;
+  border: 1px solid ${(props) => props.theme.colors.gray20};
+  background-color: ${(props) => props.theme.colors.gray5};
+  border-radius: 0.8rem;
+  cursor: pointer;
+
+  img {
+    width: 2rem;
+    height: 2rem;
+  }
+
+  .link-title {
+    width: 20rem;
+    color: ${(props) => props.theme.colors.gray90};
+    ${(props) => props.theme.fonts.caption2};
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .web-address {
+    width: 20rem;
+    color: ${(props) => props.theme.colors.gray70};
+    ${(props) => props.theme.fonts.caption3};
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+`;
+
+export const LinkBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
