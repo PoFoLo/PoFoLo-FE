b32461b750837b0290108ba6b0ab900580a5c235
letthem <suyeon3662@naver.com>
2024-12-18 20:26:31 +0900

    [Fix] access_token -> access 로 변경

--- src/pages/Login/OAuthLoading/index.tsx
+++ src/pages/Login/OAuthLoading/index.tsx
@@ -17,11 +17,11 @@ export const OAuthLoading = () => {
       try {
         const response = await instance.post('/pofolo/users/login/', { code });
 
-        const { access_token, kakao_id } = response.data;
+        const { access, kakao_id } = response.data;
 
-        if (access_token) {
+        if (access) {
           // 기존 회원: 토큰 저장 -> /home 으로 이동
-          localStorage.setItem('access_token', access_token);
+          localStorage.setItem('access', access);
           nav('/home');
         } else if (kakao_id) {
           // 신규 회원: /join 으로 이동
