d527ea8b90e28bfebaf6335b942321d17652bcf2
letthem <suyeon3662@naver.com>
2024-12-29 00:09:49 +0900

    [Fix] Navbar 수정

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
 import NavbarMobile from '@/components/Layout/Navbar/NavbarTabletMobile';
 import NavbarPC from '@/components/Layout/Navbar/NavbarPC';
 import { useResponsive } from '@/hooks/useResponsive';
 
 interface ResponsiveNavbarProps {
-  isLoggedIn: boolean;
   onGoBackClick?: () => void;
   onAboutClick?: () => void;
   onHomeClick?: () => void;
@@ -12,20 +10,17 @@ interface ResponsiveNavbarProps {
 }
 
 const Navbar = ({
-  isLoggedIn,
   onGoBackClick,
   onAboutClick,
   onHomeClick,
   onMyPageClick,
 }: ResponsiveNavbarProps) => {
-  // Media queries
-  const { isPhone } = useResponsive();
-  const { isTab } = useResponsive();
   const { isPC } = useResponsive();
+  const isLoggedIn = !!localStorage.getItem('access_token');
 
   return (
     <>
-      {(isPhone || isTab) && <NavbarMobile isLoggedIn={isLoggedIn} onGoBackClick={onGoBackClick} />}
+      {!isPC && <NavbarMobile isLoggedIn={isLoggedIn} onGoBackClick={onGoBackClick} />}
       {isPC && (
         <NavbarPC
           isLoggedIn={isLoggedIn} // PC에는 로그인 상태에 따라 로그인 or 로그아웃 버튼 렌더링 구별이 아직 없군.
