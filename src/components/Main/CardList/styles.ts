7d964489f1e5307a6d8bee9d713550c3dd182b5f
David <0815km@naver.com>
2024-12-28 13:16:56 +0900

    [Fix] Padding to Margin

--- src/components/Main/CardList/styles.ts
+++ src/components/Main/CardList/styles.ts
@@ -13,11 +13,11 @@ export const CardList = styled.div`
   width: 100%;
   max-width: 131.2rem;
   @media (max-width: 1440px) {
-    margin: 0 6.4rem;
+    margin: 2.6rem 6.4rem 0rem 6.4rem;
     width: calc(100% - 11.2rem);
   }
 
-  padding-top: 2.6rem;
+  margin-top: 2.6rem;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 2.4rem; /* 카드 간 간격 */
@@ -29,8 +29,7 @@ export const CardList = styled.div`
   ${(props) =>
     props.theme.media.ph(css`
       grid-template-columns: 1fr;
-      padding-top: 0.4rem;
-      margin: 0rem 2rem 0rem 2rem;
+      margin: 0rem 2.4rem 0rem 2rem;
       width: calc(100% - 4rem);
       flex: 1 0 0;
     `)}
@@ -39,7 +38,6 @@ export const CardList = styled.div`
     props.theme.media.tab(css`
       grid-template-columns: repeat(2, 1fr);
       gap: 2rem;
-      padding-top: 0rem;
       margin: 0rem 2rem 0rem 2rem;
       width: calc(100% - 4rem);
       flex: 1 0 0;
